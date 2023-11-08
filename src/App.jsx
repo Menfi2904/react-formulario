e85f438454e852cbadb08e0133fccf4ea70462e1
Menfi2904 <menfigarcia31@gmail.com>
2023-11-08 13:40:19 -0600

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,140 @@
 import "./App.css";
 import { registerUser } from "./services/registerUser";
+import { useForm } from "react-hook-form";
 
 export function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
+
   const onSubmit = (data) => {
     registerUser(data);
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>
             Email
-            <input type="email" placeholder="Email" />
-          </label>
-          <span className="error" role="alert">
-            Test error
-          </span>
+            <input
+              placeholder="Email"
+              type="email"
+              {...register("email", {
+                required: true,
+                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              })}
+            />
+          {errors.email?.type === "required" && (
+            <span className="error" role="alert">email is required</span>
+            )}
+          {errors.email?.type === "pattern" && (
+            <span className="error" role="alert">email is invalid</span>
+            )}
+            </label>
         </div>
+
         <div>
           <label>
             Name
-            <input type="text" placeholder="Name" />
+            <input
+              placeholder="Name"
+              type="text"
+              {...register("name", {
+                required: true,
+              })}
+            />
+          {errors.name?.type === "required" && (
+            <span className="error" role="alert">
+              name is required
+            </span>
+          )}
           </label>
-          <span className="error" role="alert"></span>
         </div>
+
         <div>
           <label>
             Age
-            <input type="number" placeholder="Age" />
+            <input
+              placeholder="Age"
+              type="number"
+              {...register("age", {
+                required: true,
+                min: 18,
+              })}
+            />
           </label>
-          <span className="error" role="alert"></span>
+          {errors.age?.type === "required" && (
+            <span className="error" role="alert">
+              age is required
+            </span>
+          )}
+          {errors.age?.type === "min" && (
+            <span className="error" role="alert">
+              you must be above 18 to register
+            </span>
+          )}
         </div>
+
         <div>
           <label>
             Password
-            <input type="password" placeholder="Password" />
+            <input
+              placeholder="Password"
+              type="password"
+              {...register("password", {
+                required: true,
+                minLength: 6,
+              })}
+            />
           </label>
-          <span className="error" role="alert"></span>
+          {errors.password?.type === "required" && (
+            <span className="error" role="alert">
+              password is required
+            </span>
+          )}
+          {errors.password?.type === "minLength" && (
+            <span className="error" role="alert">
+              password is too short
+            </span>
+          )}
         </div>
+
         <div>
           <label>
             Password check
-            <input type="password" placeholder="Password check" />
+            <input
+              placeholder="Password check"
+              type="password"
+              {...register("passwordcheck", {
+                required: true,
+                validate: (value) => value === getValues("password"),
+              })}
+            />
           </label>
-          <span className="error" role="alert"></span>
+          {errors.passwordcheck?.type === "required" && (
+            <span className="error" role="alert">
+              password check is required
+            </span>
+          )}
+          {errors.passwordcheck?.type === "validate" && (
+            <span className="error" role="alert">
+              passwords do not match
+            </span>
+          )}
         </div>
+
         <div>
           <label>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              {...register("acceptTerms", {
+                required: true,
+              })}
+            />
             Accept terms & conditions: Lorem ipsum dolor sit amet, consectetur
             adipiscing elit. Pellentesque pharetra, tortor ac placerat
             elementum, neque libero luctus mi, ut efficitur nisl mauris at nisl.
@@ -56,10 +142,16 @@ export function App() {
             magna, sollicitudin eu porttitor ut, tincidunt sit amet urna.
             Vestibulum congue neque metus.
           </label>
-          <span className="error" role="alert"></span>
+          {errors.acceptTerms?.type === "required" && (
+            <span className="error" role="alert">
+              please read and accept the terms and conditions
+            </span>
+          )}
         </div>
 
-        <button>Sign up</button>
+        <button disabled={Object.keys(errors).length > 0}>
+          Sign up
+        </button>
       </form>
     </div>
   );
